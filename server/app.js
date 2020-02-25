e0c9719c5f8b831526e04be9acf947261c1bd535
Sriram Rao <sriramudupi19.com>
2020-02-25 20:30:09 +0530

    Added mongoDB backend

--- server/app.js
+++ server/app.js
@@ -1,7 +1,16 @@
 const express = require("express");
 const graphqlHTTP = require("express-graphql");
 const schema = require("./schema/schema");
+const mongoose = require("mongoose");
+require("dotenv").config();
 const app = express();
+
+mongoose
+  .connect(process.env.DB_CONNECTION_STRING, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .catch(error => console.log(error));
 
 app.use("/graphql", graphqlHTTP({ schema, graphiql: true }));
 
